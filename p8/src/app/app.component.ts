9165436bb3c25b54fae0fd62445e2b1645e366ca
pyavchik <pyavchik@gmail.com>
2020-03-27 19:40:59 +0200

    updated

---
+++ p8/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  password = '';
+
+  onButtonClick() {
+    this.password = "New Password";
+  }
+
+  getPassword() {
+    return this.password;
+  }
+}
