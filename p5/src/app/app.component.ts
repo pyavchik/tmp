c7dd481518b4962ee64bfddfa3aab896d0f4527e
pyavchik <pyavchik@gmail.com>
2020-03-28 16:57:59 +0200

    updated

--- p5/src/app/app.component.ts
+++ p5/src/app/app.component.ts
@@ -7,12 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  private useLetters: boolean = false;
+  private useNumbers: boolean = false;
+  private useSymbols: boolean = false;
 
   onButtonClick() {
     this.password = 'My password';
+    console.log('use Letters: ', this.useLetters);
+    console.log('use Numbers: ', this.useNumbers);
+    console.log('use Symbols: ', this.useSymbols);
   }
 
   getPassword() {
     return this.password;
   }
+
+  onUseLettersChange() {
+    this.useLetters = !this.useLetters;
+  }
+
+  onUseNumbersChange() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onUseSymbolsChange() {
+    this.useSymbols = !this.useSymbols;
+  }
+
 }
