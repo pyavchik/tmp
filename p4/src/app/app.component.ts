0ef0cbe2db17d4c71f76b6eb9d7fc4af8dd07e27
pyavchik <pyavchik@gmail.com>
2020-03-28 17:11:38 +0200

    updated

--- p4/src/app/app.component.ts
+++ p4/src/app/app.component.ts
@@ -8,13 +8,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'p4';
   password = '';
+  private useLetters: boolean = false;
+  private useNumbers: boolean = false;
+  private useSymbols: boolean = false;
 
   onButtonClick() {
     console.log('Generate button clicked');
     this.password = 'My Pass'
+    console.log('use letters checkbox - ', this.useLetters);
+    console.log('use numbers checkbox - ', this.useNumbers);
+    console.log('use symbols checkbox - ', this.useSymbols);
   }
 
   getPassword() {
     return this.password;
+  }
+
+  onLettersChanged() {
+    this.useLetters = !this.useLetters;
+  }
+
+  onNumbersChanged() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onSymbolsChanged() {
+    this.useSymbols = !this.useSymbols;
   }
 }
