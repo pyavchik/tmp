805daf05bb078edadd081934749912fb74cfa123
pyavchik <pyavchik@gmail.com>
2020-03-28 19:16:42 +0200

    updated

--- p1/src/app/app.component.ts
+++ p1/src/app/app.component.ts
@@ -22,27 +22,7 @@ export class AppComponent {
   }
 
   generatePassButtonClick() {
-    const numbers = '1234567890';
-    const letters = 'abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWYZ';
-    const symbols = '!@#$%^&*()-+{}[]';
-
-    let validChars = '';
-    if (this.includeLetters) {
-      validChars += letters;
-    }
-    if (this.includeNumbers) {
-      validChars += numbers;
-    }
-    if (this.includeSymbols) {
-      validChars += symbols;
-    }
-
-    let generatedPassword = '';
-    for (let i = 0; i < this.length; i++) {
-      const index = Math.floor(Math.random() * validChars.length);
-      generatedPassword += validChars[index];
-    }
-    this.password = generatedPassword;
+    this.password = 'Password new'
   }
 
   useLettersForPass() {
