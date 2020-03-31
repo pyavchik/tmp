9cc7ac104cc7869bcb4c365b568b3e5664a9828b
pyavchik <pyavchik@gmail.com>
2020-03-31 23:40:27 +0300

    updated

--- p3103/src/app/app.component.ts
+++ p3103/src/app/app.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'p3103';
+  length: number = 9;
+  password: string = '';
+  useLetters = true;
+  useNumbers = true;
+  useSymbols = true;
+
+  onButtonClick() {
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()_+-{}[]><';
+
+    let validChars = '';
+    if (this.useLetters) {
+      validChars += letters;
+    }
+    if (this.useNumbers) {
+      validChars += numbers;
+    }
+    if (this.useSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      let index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+
+  }
+
+  onLengthChanged(event) {
+    this.length = parseInt(event.target.value)
+  }
+
+  onUseLetters() {
+    this.useLetters = !this.useLetters;
+  }
+
+  onUseNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onUseSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
 }
